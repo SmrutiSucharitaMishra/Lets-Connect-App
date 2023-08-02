ac2f5dc8b24348d32372331b509c8d830373b780
Smruti Sucharita Mishra <97398987+SmrutiSucharitaMishra@users.noreply.github.com>
2023-08-02 00:45:48 -0700

    lets connect app

---
+++ src/Pages/Room/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
+
+const RoomPage = () => {
+  const { roomId } = useParams();
+  const myMeeting = async (element) => {
+    const appID = 981785040;
+    const serverSecret = "2fa83dffdd2d8f470963113d0c15fedc";
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID,
+      serverSecret,
+      roomId,
+      Date.now().toString(),
+      "ssm"
+    );
+
+    const zc = ZegoUIKitPrebuilt.create(kitToken);
+    zc.joinRoom({
+      container: element,
+      scenario: {
+        mode: ZegoUIKitPrebuilt.OneONoneCall,
+      },
+      showScreenSharingButton: true,
+    });
+  };
+  return (
+    <div>
+      <div ref={myMeeting} />
+    </div>
+  );
+};
+
+export default RoomPage;
